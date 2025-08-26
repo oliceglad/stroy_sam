4b04417b6f51dec75b469ca7782c93265aede8a8
oliceglad <vycheslavofficial@gmail.com>
2025-08-26 13:07:57 +0300

    added addaptive for mainpage

--- src/components/PopularCategories/PopularCategories.jsx
+++ src/components/PopularCategories/PopularCategories.jsx
@@ -3,13 +3,17 @@ import { useGetMainCategoriesQuery } from "../../api/categories";
 import styles from "./PopularCategories.module.scss";
 import { CategoryCard } from "../Categories/CategoriesCard";
 import { Loader } from "../UI/Loader/Loader";
+import { useNavigate } from "react-router-dom";
 
 const PopularCategories = () => {
   const { data: categories, isLoading } = useGetMainCategoriesQuery();
+  const navigate = useNavigate();
 
   return (
     <div className={styles.popularCategories}>
-      <h2 className={styles.popularCategories__title}>Популярные категории</h2>
+      <h2 className={styles.popularCategories__title}>
+        Популярные категории <span onClick={() => navigate("/categories")}>Все категории</span>
+      </h2>
       {isLoading && <Loader />}
       <div className={styles.popularCategories__list}>
         {categories?.slice(0, 7).map((cat) => (
